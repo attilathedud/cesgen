7c11ee389a7c06571d820d1e3a6e9385e67ad14c
Nathan Tucker <abezhat@verizon.net>
2016-10-23 12:10:49 -0700

    Separating out util methods to their own class

--- cesgen/cesgen.py
+++ cesgen/cesgen.py
@@ -1,7 +1,10 @@
 from PyQt4 import QtGui
-import sys, os, shutil
+import sys, os
 
 import design
+from cesgen_utils import Cesgen_Utils
+
+# TODO: remove all os calls to cesgen_utils
 
 class CesgenApp( QtGui.QMainWindow, design.Ui_MainWindow ):
     def __init__( self ):
@@ -17,94 +20,6 @@ class CesgenApp( QtGui.QMainWindow, design.Ui_MainWindow ):
 
         if directory:
             self.leDirectory.setText( directory )
-
-    # Skeleton generation functions
-
-    # TODO: Separate out utils into another class
-    def create_file( self, file_path, str_to_write ):
-        if not os.path.exists( file_path ):
-            new_file = open( file_path, 'w' )
-
-            new_file.write( str_to_write )
-
-            new_file.flush( )
-            new_file.close( )
-
-    def create_and_copy_directory( self, project_path, dir_name, dir_to_copy ):
-        dir_path = os.path.join( project_path, dir_name )
-        if not os.path.exists( dir_path ):
-            os.makedirs( dir_path )
-
-        source_dir = dir_to_copy
-        if len( dir_to_copy ) > 0:
-            for temp_file in os.listdir( source_dir ):
-                full_temp_file_path = os.path.join( source_dir, temp_file )
-                if os.path.isfile( full_temp_file_path ):
-                    shutil.copy( full_temp_file_path, dir_path )
-
-
-    def generate_manifest_boiler( self, project_name, perm_new_tab, perm_storage, 
-                                    perm_context_menus, perm_notifications ):
-        manifest_boiler = \
-'{\n\
-    "manifest_version"  :     2,\n\
-    "name"              :     "' + project_name + '",\n\
-    "short_name"        :     "' + project_name.replace( " ", "" ) + '",\n\
-    "version"           :     "1.0.0",\n\
-    "description"       :     "' + project_name + '."' 
-
-        if perm_storage == True or perm_context_menus == True or perm_notifications == True:
-            manifest_boiler = manifest_boiler + \
-',\n\
-    "permissions" : [\n'
-
-            if perm_storage == True:
-                manifest_boiler = manifest_boiler + \
-'       "storage"'
-
-            if perm_context_menus == True:
-                manifest_boiler = manifest_boiler + \
-',\n        "contextMenus"'
-
-            if perm_notifications == True:
-                manifest_boiler = manifest_boiler + \
-',\n        "notifications"'
-
-            manifest_boiler = manifest_boiler + \
-'\n    ]'
-
-        if perm_new_tab == True:
-            manifest_boiler = manifest_boiler + \
-',\n\
-    "chrome_url_overrides" : {\n\
-        "newtab" : "main.html"\n\
-    }'
-    
-        manifest_boiler = manifest_boiler + \
-'\n\
-}\n'
-
-        return manifest_boiler
-
-    def generate_layout_boiler( self ):
-        html_boiler = \
-'<!DOCTYPE html>\n\
-<html lang="en">\n\
-<head>\n\
-	<title></title>\n\
-\n\
-    <!-- css -->\n\
-	<link rel="stylesheet" href="" type="text/css">\n\
-</head>\n\
-\n\
-<body>\n\
-\n\
-    <!-- scripts -->\n\
-	<script src=""></script>\n\
-</body>\n\
-</html>'
-
-        return html_boiler
 
     # TODO: Move this to a thread & show progress
     def generate_skeleton( self ):
@@ -124,18 +39,18 @@ class CesgenApp( QtGui.QMainWindow, design.Ui_MainWindow ):
 
             if self.chkIncludeBgScripts.isChecked( ):
                 bg_script_path = os.path.join( scripts_path, "background.js" )
-                self.create_file( bg_script_path, '' )
+                Cesgen_Utils.create_file( bg_script_path, '' )
                 
             if self.chkIncludeContentScripts.isChecked( ):
                 content_script_path = os.path.join( scripts_path, "injected.js" )
-                self.create_file( content_script_path, '' )
+                Cesgen_Utils.create_file( content_script_path, '' )
 
             if self.chkIncludeOptions.isChecked( ):
                 options_script_path = os.path.join( scripts_path, "settings.js" )
                 popup_options_script_path = os.path.join( scripts_path, "popup_settings.js" )
 
-                self.create_file( options_script_path, '' )
-                self.create_file( popup_options_script_path, '' )
+                Cesgen_Utils.create_file( options_script_path, '' )
+                Cesgen_Utils.create_file( popup_options_script_path, '' )
 
         # Create the layout section
         if self.chkIncludeLayouts.isChecked( ) or self.chkIncludeOptions.isChecked( ) or \
@@ -146,27 +61,27 @@ class CesgenApp( QtGui.QMainWindow, design.Ui_MainWindow ):
 
             if self.chkPermNewTab.isChecked( ):
                 new_tab_layout_path = os.path.join( layouts_path, "newtab.html" )
-                self.create_file( new_tab_layout_path, self.generate_layout_boiler( ) )
+                Cesgen_Utils.create_file( new_tab_layout_path, Cesgen_Utils.generate_layout_boiler( ) )
 
             if self.chkIncludeOptions.isChecked( ):
                 options_layout_path = os.path.join( layouts_path, "settings.html" )
                 popup_options_layout_path = os.path.join( layouts_path, "popup_settings.html" )
 
-                self.create_file( options_layout_path, self.generate_layout_boiler( ) )
-                self.create_file( popup_options_layout_path, self.generate_layout_boiler( ) )
+                Cesgen_Utils.create_file( options_layout_path, Cesgen_Utils.generate_layout_boiler( ) )
+                Cesgen_Utils.create_file( popup_options_layout_path, Cesgen_Utils.generate_layout_boiler( ) )
 
         # Create the css section
         if self.chkIncludeCss.isChecked():
-            self.create_and_copy_directory( project_path, "css", str( self.leIncludeCssDirectory.text() ) )
+            Cesgen_Utils.create_and_copy_directory( project_path, "css", str( self.leIncludeCssDirectory.text() ) )
 
         # Create the img section
         if self.chkIncludeImgs.isChecked():
-            self.create_and_copy_directory( project_path, "imgs", str( self.leIncludeImgsDirectory.text() ) )
+            Cesgen_Utils.create_and_copy_directory( project_path, "imgs", str( self.leIncludeImgsDirectory.text() ) )
         
         # Create the manifest file
         manifest_path = os.path.join( project_path, "manifest.json" )
-        self.create_file( manifest_path, 
-            self.generate_manifest_boiler( 
+        Cesgen_Utils.create_file( manifest_path, 
+            Cesgen_Utils.generate_manifest_boiler( 
                 str( self.leProjectName.text( ) ),
                 self.chkPermNewTab.isChecked( ),
                 self.chkPermStorage.isChecked( ),
---
+++ cesgen/cesgen_utils.py
@@ -0,0 +1,91 @@
+import sys, os, shutil
+
+class Cesgen_Utils:
+
+    @staticmethod
+    def create_file( file_path, str_to_write ):
+        if not os.path.exists( file_path ):
+            new_file = open( file_path, 'w' )
+
+            new_file.write( str_to_write )
+
+            new_file.flush( )
+            new_file.close( )
+
+    @staticmethod
+    def create_and_copy_directory( project_path, dir_name, dir_to_copy ):
+        dir_path = os.path.join( project_path, dir_name )
+        if not os.path.exists( dir_path ):
+            os.makedirs( dir_path )
+
+        source_dir = dir_to_copy
+        if len( dir_to_copy ) > 0:
+            for temp_file in os.listdir( source_dir ):
+                full_temp_file_path = os.path.join( source_dir, temp_file )
+                if os.path.isfile( full_temp_file_path ):
+                    shutil.copy( full_temp_file_path, dir_path )
+
+    @staticmethod
+    def generate_manifest_boiler( project_name, perm_new_tab, perm_storage, 
+                                    perm_context_menus, perm_notifications ):
+        manifest_boiler = \
+'{\n\
+    "manifest_version"  :     2,\n\
+    "name"              :     "' + project_name + '",\n\
+    "short_name"        :     "' + project_name.replace( " ", "" ) + '",\n\
+    "version"           :     "1.0.0",\n\
+    "description"       :     "' + project_name + '."' 
+
+        if perm_storage == True or perm_context_menus == True or perm_notifications == True:
+            manifest_boiler = manifest_boiler + \
+',\n\
+    "permissions" : [\n'
+
+            if perm_storage == True:
+                manifest_boiler = manifest_boiler + \
+'        "storage"'
+
+            if perm_context_menus == True:
+                manifest_boiler = manifest_boiler + \
+',\n        "contextMenus"'
+
+            if perm_notifications == True:
+                manifest_boiler = manifest_boiler + \
+',\n        "notifications"'
+
+            manifest_boiler = manifest_boiler + \
+'\n    ]'
+
+        if perm_new_tab == True:
+            manifest_boiler = manifest_boiler + \
+',\n\
+    "chrome_url_overrides" : {\n\
+        "newtab" : "main.html"\n\
+    }'
+    
+        manifest_boiler = manifest_boiler + \
+'\n\
+}\n'
+
+        return manifest_boiler
+
+    @staticmethod
+    def generate_layout_boiler( ):
+        html_boiler = \
+'<!DOCTYPE html>\n\
+<html lang="en">\n\
+<head>\n\
+	<title></title>\n\
+\n\
+    <!-- css -->\n\
+	<link rel="stylesheet" href="" type="text/css">\n\
+</head>\n\
+\n\
+<body>\n\
+\n\
+    <!-- scripts -->\n\
+	<script src=""></script>\n\
+</body>\n\
+</html>'
+
+        return html_boiler
